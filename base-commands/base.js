c93859e8ba2cc4aa401c406e21515492cc9cba5e
Zack <zackmeyer1@hotmail.com>
2019-07-27 15:06:21 -0600

    Polished up the role commands

--- base-commands/base.js
+++ base-commands/base.js
@@ -7,18 +7,27 @@ const addRole = (receivedMessage, role) => {
 
     const teRole = receivedMessage.guild.roles.find(teRole => teRole.name === role);
 
-    receivedMessage.member.addRole(teRole).then(() => {
-        receivedMessage.channel.send(`${receivedMessage.author}, you have been given the "${role}" role.`).catch((err) => {
-            client.fetchUser('400191346742263818').then((user) => { //Zealot's ID
-                user.send(`I borked.  Message: ${receivedMessage.content} \n Error: ${err}`);
+    if(!teRole) {
+        receivedMessage.channel.send(`${receivedMessage.author}, the ${role} role doesn't seem to exist.  Make sure you spelled it right.  I am case-sensitive, so make sure the casing matches, too.`);
+        return;
+    }
+
+    if (receivedMessage.member.roles.has(teRole.id)) {
+        receivedMessage.channel.send(`${receivedMessage.author}, you already have the "${role}" role!`);
+    } else {
+        receivedMessage.member.addRole(teRole).then(() => {
+            receivedMessage.channel.send(`${receivedMessage.author}, you have been given the "${role}" role.`).catch((err) => {
+                client.fetchUser('400191346742263818').then((user) => { //Zealot's ID
+                    user.send(`I borked.  Message: ${receivedMessage.content} \n Error: ${err}`);
+                });
+                client.fetchUser('358333674514677760').then((user) => { //Arken's ID
+                    user.send(`I borked.  Message: ${receivedMessage.content} \n Error: ${err}`);
+                });
             });
-            client.fetchUser('358333674514677760').then((user) => { //Arken's ID
-                user.send(`I borked.  Message: ${receivedMessage.content} \n Error: ${err}`);
-            });
+        }).catch((err) => {
+            receivedMessage.channel.send('Failed to add the role.');
         });
-    }).catch((err) => {
-        receivedMessage.channel.send('Failed to add the role.  You either spelled it wrong or tried to add a role only the mods can give you.');
-    });
+    }
 }
 
 const removeRole = (receivedMessage, role) => {
@@ -30,18 +39,27 @@ const removeRole = (receivedMessage, role) => {
 
     const teRole = receivedMessage.guild.roles.find(teRole => teRole.name === role);
 
-    receivedMessage.member.removeRole(teRole).then(() => {
-        receivedMessage.channel.send(`${receivedMessage.author}, I've removed the "${role}" role from you.`).catch((err) => {
-            client.fetchUser('400191346742263818').then((user) => { //Zealot's ID
-                user.send(`I borked.  Message: ${receivedMessage.content} \n Error: ${err}`);
+    if(!teRole) {
+        receivedMessage.channel.send(`${receivedMessage.author}, the ${role} role doesn't seem to exist.  Make sure you spelled it right.  I am case-sensitive, so make sure the casing matches, too.`);
+        return;
+    }
+
+    if (receivedMessage.member.roles.has(teRole.id)) {
+        receivedMessage.member.removeRole(teRole).then(() => {
+            receivedMessage.channel.send(`${receivedMessage.author}, I've removed the "${role}" role from you.`).catch((err) => {
+                client.fetchUser('400191346742263818').then((user) => { //Zealot's ID
+                    user.send(`I borked.  Message: ${receivedMessage.content} \n Error: ${err}`);
+                });
+                client.fetchUser('358333674514677760').then((user) => { //Arken's ID
+                    user.send(`I borked.  Message: ${receivedMessage.content} \n Error: ${err}`);
+                });
             });
-            client.fetchUser('358333674514677760').then((user) => { //Arken's ID
-                user.send(`I borked.  Message: ${receivedMessage.content} \n Error: ${err}`);
-            });
+        }).catch((err) => {
+            receivedMessage.channel.send('Failed to remove the role.');
         });
-    }).catch((err) => {
-        receivedMessage.channel.send('Failed to remove the role.  Make sure you spelled it right.');
-    });
+    } else {
+        receivedMessage.channel.send(`${receivedMessage.author}, you don't have the "${role}" role!`);
+    }
 }
 
 module.exports = {
