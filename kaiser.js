74ab0f0a378ed122e2c0778e808a247b057321aa
Zack <zackmeyer1@hotmail.com>
2019-09-04 22:54:23 -0600

    Added try...catch around processCommand

--- kaiser.js
+++ kaiser.js
@@ -34,45 +34,49 @@ client.on('messageReactionAdd', (messageReaction) => {
 });
 
 const processCommand = (receivedMessage) => {
-	let fullCommand = receivedMessage.content.substr(1) // Remove the leading character
-	let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
-	let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
-	primaryCommand = primaryCommand.toLowerCase();  //make the command lower case to eliminate case sensitivity
-	let args = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
+	try {
+		let fullCommand = receivedMessage.content.substr(1) // Remove the leading character
+		let splitCommand = fullCommand.split(" ") // Split the message up in to pieces for each space
+		let primaryCommand = splitCommand[0] // The first word directly after the exclamation is the command
+		primaryCommand = primaryCommand.toLowerCase();  //make the command lower case to eliminate case sensitivity
+		let args = splitCommand.slice(1) // All other words are arguments/parameters/options for the command
 
-	switch(primaryCommand) {
-		case 'cooldudes':
-			misc.cooldudes(receivedMessage);
-			break;
-		case 'bamboozled':
-			misc.bamboozled(receivedMessage);
-			break;
-		case 'illegal':
-			misc.illegal(receivedMessage);
-			break;
-		case 'addrole':
-			base.addRole(receivedMessage, args);
-			break;
-		case 'removerole':
-			base.removeRole(receivedMessage, args);
-			break;
-		case 'addroles':
-			base.addRoles(receivedMessage, args);
-			break;
-		case 'removeroles':
-			base.removeRoles(receivedMessage, args);
-			break;
-		case 'info':
-			base.info(receivedMessage, args);
-			break;
-		case 'help':
-			base.help(receivedMessage);
-			break;
-		case 'roles':
-			base.roles(receivedMessage);
-			break;
-		default:
-			receivedMessage.channel.send('Invalid command.');
+		switch(primaryCommand) {
+			case 'cooldudes':
+				misc.cooldudes(receivedMessage);
+				break;
+			case 'bamboozled':
+				misc.bamboozled(receivedMessage);
+				break;
+			case 'illegal':
+				misc.illegal(receivedMessage);
+				break;
+			case 'addrole':
+				base.addRole(receivedMessage, args);
+				break;
+			case 'removerole':
+				base.removeRole(receivedMessage, args);
+				break;
+			case 'addroles':
+				base.addRoles(receivedMessage, args);
+				break;
+			case 'removeroles':
+				base.removeRoles(receivedMessage, args);
+				break;
+			case 'info':
+				base.info(receivedMessage, args);
+				break;
+			case 'help':
+				base.help(receivedMessage);
+				break;
+			case 'roles':
+				base.roles(receivedMessage);
+				break;
+			default:
+				receivedMessage.channel.send('Invalid command.');
+		}
+	} catch (err) {
+		base.sendError(receivedMessage, err);
 	}
 }
 
