da5d3005b1172b7f08147e5282d9b593f709869a
ArkenStorm <shadowarchon7@gmail.com>
2020-12-19 15:19:32 -0700

    use the right db function silly

--- misc-commands/tracking.js
+++ misc-commands/tracking.js
@@ -3,7 +3,7 @@ const track = async (context) => {
 	await context.nosql.get('tracking')
 		.push({'userID': context.message.author.id, 'serverID': context.message.guild.id, 'statistic': trackString, 'value': 0})
 		.write();
-	await context.message.channel.send(`Now tracking \`${trackString}\` for you, ${context.message.author.name}`);
+	await context.message.channel.send(`Now tracking \`${trackString}\` for you, ${context.message.author.username}`);
 }
 
 const trackUpdate = async (context) => {
@@ -16,14 +16,14 @@ const trackUpdate = async (context) => {
 		.find({'userID': context.message.author.id, 'statistic': trackString})
 		.assign({'value': updateNum})
 		.write();
-	await context.message.channel.send(`Updated \`${trackString}\` for you, ${context.message.author.name}`);
+	await context.message.channel.send(`Updated \`${trackString}\` for you, ${context.message.author.username}`);
 }
 
 const listTrackedItems = async (context) => {
 	const trackedItems = await context.nosql.get('tracking')
 		.find({'userID': context.message.author.id})
 		.value();
-	let trackEmbed = new Discord.MessageEmbed().setColor('#2295d4').setTitle(`${context.message.author.name}'s tracked items:`);
+	let trackEmbed = new Discord.MessageEmbed().setColor('#2295d4').setTitle(`${context.message.author.username}'s tracked items:`);
 	trackedItems.forEach(item => {
 		trackEmbed.addField(item.statistic, item.value);
 	});
@@ -35,13 +35,13 @@ const stopTracking = async (context) => {
 	await context.nosql.get('tracking')
 		.remove({'userID': context.message.author.id, 'statistic': trackString})
 		.write();
-	await context.message.channel.send(`No longer tracking \`${trackString}\` for you, ${context.message.author.name}`);
+	await context.message.channel.send(`No longer tracking \`${trackString}\` for you, ${context.message.author.username}`);
 }
 
 const leaderboard = async (context) => {
 	let trackString = context.args.join(' ').toLowerCase();
 	let leaderboardItems = await context.nosql.get('tracking')
-		.find({'serverID': context.message.guild.id, 'statistic': trackString})
+		.filter({'serverID': context.message.guild.id, 'statistic': trackString})
 		.value();
 	let leaderboardEmbed = new Discord.MessageEmbed().setColor('#2295d4').setTitle(`Leaderboard for ${trackString}`);
 	leaderboardItems.forEach(item => {
